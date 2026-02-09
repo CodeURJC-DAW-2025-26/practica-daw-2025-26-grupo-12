c16718afec5ce32eef004027f547f410c9a4dcb3
Marcelo Domínguez <dmmarcelo27@gmail.com>
2026-02-09 11:34:44 +0100

    Initialize Spring project

---
+++ src/main/java/es/codeurjc/grupo12/scissors_please/HelloController.java
@@ -0,0 +1,14 @@
+package es.codeurjc.grupo12.scissors_please;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/es/codeurjc/grupo12/scissors_please/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.grupo12.scissors_please;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
