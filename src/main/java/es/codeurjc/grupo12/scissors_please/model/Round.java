73ca9a9da97ba584e16eb7991c58b043fe44df0b
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2026-02-17 23:39:36 +0100

    Little fixes

--- src/main/java/es/codeurjc/grupo12/scissors_please/model/Round.java
+++ src/main/java/es/codeurjc/grupo12/scissors_please/model/Round.java
@@ -1,6 +1,9 @@
 package es.codeurjc.grupo12.scissors_please.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,6 +11,11 @@ import lombok.Data;
 @Data
 @Entity
 public class Round {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
   private int roundNumber;
   private String bot1Move;
   private String bot2Move;
--- src/main/java/es/codeurjc/grupo12/scissors_please/model/Tournament.java
+++ src/main/java/es/codeurjc/grupo12/scissors_please/model/Tournament.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -21,6 +22,8 @@ public class Tournament {
   private String name;
   private String description;
   private String status;
+  
+  @OneToOne
   private User creator;
 
   @OneToMany
