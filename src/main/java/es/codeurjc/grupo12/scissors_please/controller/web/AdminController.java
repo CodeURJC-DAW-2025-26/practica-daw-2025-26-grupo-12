48dba264d4034e69d185c7653694c6e0b33f2dd2
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2026-02-28 13:12:50 +0100

    import clean

--- src/main/java/es/codeurjc/grupo12/scissors_please/controller/web/AdminController.java
+++ src/main/java/es/codeurjc/grupo12/scissors_please/controller/web/AdminController.java
@@ -1,18 +1,11 @@
 package es.codeurjc.grupo12.scissors_please.controller.web;
 
-import es.codeurjc.grupo12.scissors_please.model.User;
-import es.codeurjc.grupo12.scissors_please.security.ActiveSessionService;
-import es.codeurjc.grupo12.scissors_please.service.TournamentAutomationService;
-import es.codeurjc.grupo12.scissors_please.service.TournamentService;
-import es.codeurjc.grupo12.scissors_please.service.UserService;
-import es.codeurjc.grupo12.scissors_please.service.UserService.UserStatusFilter;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +20,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriUtils;
+
+import es.codeurjc.grupo12.scissors_please.model.User;
+import es.codeurjc.grupo12.scissors_please.security.ActiveSessionService;
+import es.codeurjc.grupo12.scissors_please.service.TournamentAutomationService;
+import es.codeurjc.grupo12.scissors_please.service.TournamentService;
+import es.codeurjc.grupo12.scissors_please.service.UserService;
+import es.codeurjc.grupo12.scissors_please.service.UserService.UserStatusFilter;
 
 @Controller
 @RequestMapping("/admin")
