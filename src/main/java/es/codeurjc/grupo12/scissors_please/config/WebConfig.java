2814ebff576eb7d40987accbd21c7b05f222a30e
Lamoara <jorginos40@gmail.com>
2026-03-01 14:47:28 +0100

    Matchmaking queue and rematch flow

---
+++ src/main/java/es/codeurjc/grupo12/scissors_please/config/WebConfig.java
@@ -0,0 +1,20 @@
+package es.codeurjc.grupo12.scissors_please.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Autowired private MatchmakingLockInterceptor matchmakingLockInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry
+        .addInterceptor(matchmakingLockInterceptor)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/css/**", "/js/**", "/images/**", "/tournament-images/**");
+  }
+}
