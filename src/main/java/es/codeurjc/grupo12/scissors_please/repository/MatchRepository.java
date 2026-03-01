02365ed2fc05a19931779cf59b33aa3addf9b65e
Marcelo Domínguez <dmmarcelo27@gmail.com>
2026-03-01 13:17:15 +0100

    Advanced query for best matches

--- src/main/java/es/codeurjc/grupo12/scissors_please/repository/MatchRepository.java
+++ src/main/java/es/codeurjc/grupo12/scissors_please/repository/MatchRepository.java
@@ -19,10 +19,32 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
       left join m.bot1 b1
       left join m.bot2 b2
       order by
-        case
-          when coalesce(b1.elo, 0) >= coalesce(b2.elo, 0) then coalesce(b1.elo, 0)
-          else coalesce(b2.elo, 0)
-        end desc,
+        (
+          case
+            when (b1.elo < b2.elo and m.bot1Score > m.bot2Score)
+              or (b2.elo < b1.elo and m.bot2Score > m.bot1Score)
+            then least(abs(b1.elo - b2.elo) / 400, 1.0)
+            else -least(abs(b1.elo - b2.elo) / 400, 1.0)
+          end
+
+          +
+
+          least(((coalesce(b1.elo, 0) + coalesce(b2.elo, 0)) / 2.0) / 4000.0, 1.0)
+
+          +
+
+          least(
+            (
+              select count(m2)
+              from Match m2
+              where
+              (m2.bot1 = b1 and m2.bot2 = b2)
+              or
+              (m2.bot1 = b2 and m2.bot2 = b1)
+            ) / 10.0,
+            1.0
+          )
+        ) desc,
         m.timestamp desc,
         m.id desc
       """)
