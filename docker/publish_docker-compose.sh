5015fd0eb7bf686a70e3a444239d00455a7e4471
Marcelo Domínguez <dmmarcelo27@gmail.com>
2026-03-18 15:01:09 +0100

    Deploy app with https on the server

--- docker/publish_docker-compose.sh
+++ docker/publish_docker-compose.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <image-name>"
+if [ -z "$2" ]; then
+    echo "Usage: $0 <image-name> <web-image-name>"
     exit 1
 fi
 
 IMAGE_NAME=$1
+WEB_IMAGE_VALUE=$2
 
-WEB_IMAGE=sa4dus/scissors-please envsubst < docker/docker_compose.yml > docker/docker-compose.resolved.yml
+echo "IMAGE_NAME: $IMAGE_NAME"
+echo "WEB_IMAGE: $WEB_IMAGE_VALUE"
+
+export IMAGE_NAME=$IMAGE_NAME
+export WEB_IMAGE=$WEB_IMAGE_VALUE
+
+envsubst < docker/docker_compose.yml > docker/docker-compose.resolved.yml
 docker compose -f docker/docker-compose.resolved.yml publish $IMAGE_NAME --with-env
