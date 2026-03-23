2ba14d346271d0825857583be6e35f5f091bf3f1
Marcelo Domínguez <dmmarcelo27@gmail.com>
2026-03-23 10:56:22 +0100

    Fix infinite error loop on REST failure

--- backend/src/main/java/es/codeurjc/grupo12/scissors_please/controller/api/v1/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/es/codeurjc/grupo12/scissors_please/controller/api/v1/exceptions/GlobalExceptionHandler.java
@@ -9,11 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
