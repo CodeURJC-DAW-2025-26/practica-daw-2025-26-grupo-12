4c2891684c1c9424053fb41b6f282aea0cf0fa62
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2026-04-12 20:29:36 +0200

    React with our styles

---
+++ frontend/app/components/header.tsx
@@ -0,0 +1,41 @@
+interface HeaderProps{logged:boolean,admin:boolean}
+export default function Header({logged,admin}:HeaderProps){
+    return(
+        <>
+    <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
+    <div className="container">
+        <a className="navbar-brand" href="/">✂️ Scissors, Please</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item"><a className="nav-link" href="/home">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="/matches/list">Best Matches</a></li>
+                {admin && <><li className="nav-item"><a className="nav-link" href="/admin/tournaments">Tournaments</a></li>
+                <li className="nav-item"><a className="nav-link" href="/admin/users">Users</a></li>
+                <li className="nav-item"><a className="nav-link" href="/admin/bots">Bots</a></li></>}
+                
+                {!admin && <><li className="nav-item"><a className="nav-link" href="/tournaments">Tournaments</a></li></>}
+            </ul>
+            <div className="d-flex gap-2 align-items-center">
+                {logged && admin && <span className="badge bg-primary me-2">Admin</span>}
+   
+                {logged && !admin && <a href="/user/profile" className="btn btn-outline-muted btn-sm px-3">Profile</a>}
+
+                {logged && <><form action="/logout" method="post" className="d-inline">
+                    <input type="hidden" name="_csrf" value="{{_csrf.token}}" />
+                    <button type="submit" className="btn btn-primary btn-sm px-3">Log Out</button>
+                </form></>}
+                
+                {!logged && <> <a href="/login" className="btn btn-outline-muted btn-sm px-3">Log In</a>
+                <a href="/sign-up" className="btn btn-primary btn-sm px-3">Sign Up</a> </>}
+            </div>
+        </div>
+    </div>
+    </nav>
+    </>
+    )
+
+
+}
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,5 @@
+import Header from "~/components/header";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +8,6 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (<Header admin={true} logged={true} />
+  )
 }
